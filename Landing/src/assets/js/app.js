e12cb6059ea373929c61b012acdf3abc58828502
Your Name <your_email@example.com>
2026-06-20 09:33:41 +0530

    Initial commit

---
+++ Landing/src/assets/js/app.js
@@ -0,0 +1,27 @@
+/* Theme Name: Appexy - Responsive bootsrap 4 Landing Page project
+   Author: Coderthemes
+   Version: 1.0.0
+   Created: May 2018
+   File Description: Main Js file of the project
+*/
+
+
+
+// Sticky Navbar
+
+function windowScroll() {
+    const navbar = document.getElementById("navbar-sticky");
+    if (navbar) {
+        if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+            navbar.classList.add("nav-sticky");
+        } else {
+            navbar.classList.remove("nav-sticky");
+        }
+    }
+}
+
+window.addEventListener("scroll", (ev) => {
+    ev.preventDefault();
+    windowScroll();
+});
+
